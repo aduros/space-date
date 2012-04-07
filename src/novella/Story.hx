2ebe98b42ea5d3fefe1c1dcb52f27e7ebadf698c
Bruno Garcia <b@aduros.com>
2012-04-07 14:50:16 -0700

    Indent for readability.

--- src/novella/Story.hx
+++ src/novella/Story.hx
@@ -2,20 +2,46 @@ package novella;
 
 class Story
 {
-    public static var begin = new Screen()
-        .at(Office).with(MarkBashful).saying("This is a test!").playing(Jazz).then()
-        .with(MarkHappy).saying("This text is very, very long and should automatically"
-            + " wrap into multiple lines. Cuz it's long. Now for a moment of silence.").then()
-        .with(Mark).playing(Silence).then()
-        .with(MarkMigraine).playing(Jazz).saying("Whoa, I feel a dilemma coming up...").then()
-        .choice("Let Mark go home early?"
-        , "Yes", new Screen()
-            .with(MarkHappy).saying("Finally, it's over!").playing(Chiptune).then()
-            .saying("Thanks for playing").then()
-            .with(Nobody).saying("And so I left the office never to be seen again...").then()
-            .then()
-            .at(PlaceKitten).with(Mark).saying("I CHOOSE YOU, GIANT PLACEHOLDER KITTEN")
-        , "Nope", new Screen()
-            .with(Mark).saying("Well thanks for nothing. Reload and try again, meanie.")
-        );
+    public static var begin =
+        branch()
+            .at(Office)
+            .with(MarkBashful)
+            .saying("This is a test!")
+            .playing(Jazz)
+        .then()
+            .with(MarkHappy)
+            .saying("This text is very, very long and should automatically"
+                + " wrap into multiple lines. Cuz it's long. Now for a moment of silence.")
+        .then()
+            .with(Mark)
+            .playing(Silence)
+        .then()
+            .with(MarkMigraine)
+            .playing(Jazz)
+            .saying("Whoa, I feel a dilemma coming up...")
+        .then()
+            .choice("Let Mark go home early?"
+            , "Yes", branch()
+                    .with(MarkHappy)
+                    .saying("Finally, it's over!")
+                    .playing(Chiptune)
+                .then()
+                    .saying("Thanks for playing")
+                .then()
+                    .with(Nobody)
+                    .saying("And so I left the office never to be seen again...")
+                .then()
+                .then()
+                    .at(PlaceKitten)
+                    .with(Mark)
+                    .saying("I CHOOSE YOU, GIANT PLACEHOLDER KITTEN")
+            , "Nope", branch()
+                    .with(Mark)
+                    .saying("Well thanks for nothing. Reload and try again, meanie.")
+            );
+
+    public static function branch ()
+    {
+        return new Screen();
+    }
 }
