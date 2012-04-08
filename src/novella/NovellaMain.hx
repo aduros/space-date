5dc1a11fe5761a6eacb45522fa6de0c3e330e4a5
Bruno Garcia <b@aduros.com>
2012-04-07 18:02:11 -0700

    Add preloader.

--- src/novella/NovellaMain.hx
+++ src/novella/NovellaMain.hx
@@ -1,10 +1,5 @@
 package novella;
 
-import flambe.asset.AssetPack;
-import flambe.asset.Manifest;
-import flambe.display.FillSprite;
-import flambe.display.Sprite;
-import flambe.Entity;
 import flambe.scene.Director;
 import flambe.System;
 
@@ -16,18 +11,9 @@ class NovellaMain
         System.init();
 
         System.root.add(new Director());
+        System.stage.lockOrientation(Landscape);
 
-        // Load up the compiled pack in the assets directory named "bootstrap"
-        var manifest = Manifest.build("bootstrap");
-        var loader = System.loadAssetPack(manifest);
-        loader.get(onSuccess);
-    }
-
-    private static function onSuccess (pack :AssetPack)
-    {
-        var ctx = new NovellaCtx(pack);
-        var mainScene = MainScene.create(ctx);
-
-        System.root.get(Director).unwindToScene(mainScene);
+        var preloader = PreloaderScene.create();
+        System.root.get(Director).unwindToScene(preloader);
     }
 }
---
+++ src/novella/PreloaderScene.hx
@@ -0,0 +1,49 @@
+package novella;
+
+import flambe.asset.AssetPack;
+import flambe.asset.Manifest;
+import flambe.display.FillSprite;
+import flambe.display.Sprite;
+import flambe.Entity;
+import flambe.scene.Director;
+import flambe.System;
+
+class PreloaderScene
+{
+    public static function create () :Entity
+    {
+        var scene = new Entity();
+
+        var background = new Entity()
+            .add(new FillSprite(0x202020, System.stage.width, System.stage.height));
+        scene.addChild(background);
+
+        var manifest = Manifest.build("bootstrap");
+        var loader = System.loadAssetPack(manifest);
+        loader.get(onSuccess);
+
+        var progressWidth = 500;
+        var progressBar = new Entity()
+            .add(new FillSprite(0x000000, progressWidth, 20)
+                .centerAnchor()
+                .setXY(System.stage.width/2, System.stage.height/2));
+        scene.addChild(progressBar);
+
+        var barFill = new Entity().add(new FillSprite(0xffffff, 0, 20));
+        loader.progressChanged.connect(function () {
+            var ratio = loader.progress/loader.total;
+            barFill.get(FillSprite).width._ = ratio*progressWidth;
+        });
+        progressBar.addChild(barFill);
+
+        return scene;
+    }
+
+    private static function onSuccess (pack :AssetPack)
+    {
+        var ctx = new NovellaCtx(pack);
+        var mainScene = MainScene.create(ctx);
+
+        System.root.get(Director).unwindToScene(mainScene);
+    }
+}
