250bc2faee9070b361b89d8add75ae76bd686996
Bruno Garcia <b@aduros.com>
2012-04-07 20:50:34 -0700

    Pause on periods.

--- src/novella/TypeAction.hx
+++ src/novella/TypeAction.hx
@@ -4,6 +4,8 @@ import flambe.script.Action;
 import flambe.display.TextSprite;
 import flambe.Entity;
 
+using StringTools;
+
 /**
  * An Action that animates text being typed character by character.
  */
@@ -12,8 +14,10 @@ class TypeAction implements Action
     public function new (text :String, msPerChar :Int, ?spriteOverride :TextSprite)
     {
         _text = text;
-        _duration = text.length * msPerChar;
+        _msPerChar = msPerChar;
         _spriteOverride = spriteOverride;
+
+        reset();
     }
 
     public function update (dt :Int, actor :Entity) :Bool
@@ -24,21 +28,42 @@ class TypeAction implements Action
         }
 
         _elapsed += dt;
-        if (_elapsed >= _duration) {
-            sprite.text = _text;
-            _elapsed = 0;
-            return true;
+        while (_elapsed >= _delay) {
+            _elapsed -= _delay;
+
+            if (_charIdx >= _text.length) {
+                reset();
+                return true;
+            }
+            sprite.text = _text.substr(0, _charIdx+1);
+            _delay = _msPerChar * charDelay(_text.fastCodeAt(_charIdx));
+            ++_charIdx;
         }
 
-        var n = Std.int(_text.length * _elapsed/_duration);
-        if (n != sprite.text.length) {
-            sprite.text = _text.substr(0, n);
-        }
         return false;
+    }
+
+    public function reset ()
+    {
+        _elapsed = 0;
+        _delay = 0;
+        _charIdx = -1;
+    }
+
+    public static function charDelay (charCode :Int) :Int
+    {
+        // Sentence delimitters cause the animation to pause for a moment
+        switch (charCode) {
+            case '.'.code, '!'.code, '?'.code: return 20;
+            default: return 1;
+        }
     }
 
     private var _text :String;
     private var _spriteOverride :TextSprite;
+    private var _msPerChar :Int;
+
     private var _elapsed :Int;
-    private var _duration :Int;
+    private var _delay :Int;
+    private var _charIdx :Int;
 }
