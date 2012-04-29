8026f46b649bdd1b9f853a57977cb57a20d6a554
Bruno Garcia <b@aduros.com>
2012-04-28 21:23:18 -0700

    Show unlocked endings on the title screen.

--- src/novella/MainScene.hx
+++ src/novella/MainScene.hx
@@ -1,7 +1,7 @@
 package novella;
 
-import flambe.display.Sprite;
 import flambe.display.ImageSprite;
+import flambe.display.Sprite;
 import flambe.Entity;
 import flambe.scene.Director;
 import flambe.System;
@@ -20,7 +20,10 @@ class MainScene
             System.root.get(Director).unwindToScene(StoryScene.create(ctx));
         });
 
-        trace("Unlocked endings: " + ctx.endings);
+        var unlockedEndings = Math.min(ctx.endings.length, 4);
+        scene.addChild(new Entity()
+            .add(new ImageSprite(ctx.pack.loadTexture("endings" + unlockedEndings + ".png"))
+                .setXY(23, 434)));
 
         return scene;
     }
