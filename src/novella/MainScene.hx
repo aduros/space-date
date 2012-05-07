b83bb7cc1eb417cad8f98d083f4b2dafb9e6ceb1
Bruno Garcia <b@aduros.com>
2012-05-06 20:18:14 -0700

    Animate the title screen a bit.

--- src/novella/MainScene.hx
+++ src/novella/MainScene.hx
@@ -1,5 +1,6 @@
 package novella;
 
+import flambe.animation.Easing;
 import flambe.display.ImageSprite;
 import flambe.display.Sprite;
 import flambe.Entity;
@@ -14,6 +15,19 @@ class MainScene
         var background = new Entity()
             .add(new ImageSprite(ctx.pack.loadTexture("title.jpg")));
         scene.addChild(background);
+
+        var planet = new Entity()
+            .add(new ImageSprite(ctx.pack.loadTexture("planet.png")).centerAnchor())
+            .add(new PlanetMotion());
+        scene.addChild(planet);
+
+        var logo = new Entity()
+            .add(new ImageSprite(ctx.pack.loadTexture("logo.png"))
+                .centerAnchor().setXY(NovellaConsts.WIDTH/2, 150));
+        var sprite = logo.get(Sprite);
+        sprite.scaleX.animate(0, 1, 0.8, Easing.backOut);
+        sprite.scaleY.animate(0, 1, 0.8, Easing.backOut);
+        scene.addChild(logo);
 
         var unlockedEndings = Math.min(ctx.endings.length, 4);
         scene.addChild(new Entity()
@@ -30,4 +44,25 @@ class MainScene
 
         return scene;
     }
+}
+
+private class PlanetMotion extends flambe.Component
+{
+    public function new ()
+    {
+        _elapsed = 5000; // Start a few seconds in
+    }
+
+    override public function onUpdate (dt :Int)
+    {
+        _elapsed += dt;
+
+        var speed = 0.0001;
+        var sprite = owner.get(Sprite);
+        sprite.x._ = NovellaConsts.WIDTH/2 + 200*Math.sin(1.5*speed*_elapsed);
+        sprite.y._ = 250 + 100*Math.sin(speed*_elapsed);
+        sprite.scaleX._ = sprite.scaleY._ = 1 + 0.2*Math.sin(speed*_elapsed);
+    }
+
+    private var _elapsed :Int;
 }
